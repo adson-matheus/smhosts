3944d44ce1e746377bd14c5adf3930737a6116ad
willamylp <willamy.wlp@gmail.com>
2019-05-27 19:04:57 -0300

    Ajustes na Listagem de Hosts

--- Hosts/models.py
+++ Hosts/models.py
@@ -27,5 +27,10 @@ class Hosts(models.Model):
         max_length=200,
         blank=False
     )
+    status = models.BooleanField(
+        default=False
+    )
     
+    def __str__(self):
+        return self.hostname
     
