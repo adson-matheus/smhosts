4ff4ec6ec6391c9d9cb9735566cf7ce506ef2e3a
Adson Matheus <20200023325@emcm.caico>
2021-11-23 11:02:48 -0300

    grafico da tela principal

--- Principal/views.py
+++ Principal/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from Hosts.models import Host
+from Hosts.models import Evento
 
 import json
 
 @login_required
 def principal(request):
-    hosts = Host.objects.all()
-    return render(request, 'principal.html', {'hosts': hosts})
+    listaPing = []
+    listaHosts = []
+    eventos = Evento.objects.all()
+    for e in eventos:
+        listaPing.append(e.ping)
+        listaHosts.append(e.host_porta_id.host.hostname)
+    #listaPing.remove(None)
+    return render(request, 'principal.html', {'eventos': eventos, 'listaPing':listaPing, 'listaHosts':listaHosts})
