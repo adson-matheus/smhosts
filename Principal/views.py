7edc61dd7ccce9008611ac1d0cddca409704d9e0
Adson Matheus <20200023325@emcm.caico>
2021-11-24 09:47:33 -0300

    zingchart e editando icones

--- Principal/views.py
+++ Principal/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from Hosts.models import Evento
+from Hosts.models import Evento, Host_Porta
 
+from Hosts.views import verificaServer
 import json
 
 @login_required
 def principal(request):
+    hosts = Host_Porta.objects.all()
     listaPing = []
     listaHosts = []
+    #toda vez que atualizar a pagina inicial, verifica o ping
+    for h in hosts:
+        verificaServer(h.id)
+
     eventos = Evento.objects.all()
     for e in eventos:
         listaPing.append(e.ping)
