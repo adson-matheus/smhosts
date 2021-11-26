4bfa9d42d27a42b630c659c72861e4e5e0396030
Adson Matheus <20200023325@emcm.caico>
2021-11-26 16:59:55 -0300

    novo grafico com linha do tempo do ping [nao finalizado]

--- Principal/views.py
+++ Principal/views.py
@@ -8,13 +8,21 @@ import json
 @login_required
 def principal(request):
     hosts = Host_Porta.objects.all()
+    eventos = Evento.objects.all()
     listaPing = []
     listaHosts = []
+    pingTeste = []
+    pingsAll = []
+    
     #toda vez que atualizar a pagina inicial, verifica o ping
     for h in hosts:
         verificaServer(h.id)
 
-    eventos = Evento.objects.all()
+    #guarda tudo em pingsAll
+    for e in eventos:
+        pingTeste = retornaPing(e.id)
+        pingsAll.append(pingTeste)
+
     for e in eventos:
         if e.ping == None:
             pass
@@ -22,4 +30,14 @@ def principal(request):
             listaPing.append(e.ping)
             listaHosts.append(e.host_porta_id.host.id)
 
-    return render(request, 'principal.html', {'eventos': eventos, 'listaPing':listaPing, 'listaHosts':listaHosts})
+    return render(request, 'principal.html', {'eventos': eventos, 'listaPing':listaPing, 'listaHosts':listaHosts, 'pingTeste':pingTeste, 'pingsAll':pingsAll})
+
+def retornaPing(id):
+    #retorna todos os pings ja feitos
+    #envia para o grafico
+    evento = Evento.objects.get(pk=id)
+    historicoEvento = evento.history.all()
+    listaPing = []
+    for e in historicoEvento:
+        listaPing.append(e.ping)
+    return listaPing
