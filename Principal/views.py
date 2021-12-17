ba1c595e9b01d1de6db4a50e428c9c473b58a8b3
adson62 <adson.matheus62@gmail.com>
2021-12-17 16:54:13 -0300

    criacao da funcao datas [erro]; tentando mostrar None do python como null no js

--- Principal/views.py
+++ Principal/views.py
@@ -9,34 +9,51 @@ def principal(request):
     eventos = Evento.objects.all()
     histPingHost = []
     histTodosHosts = []
-    dataHora = []
     maxValorGraf = []
     passo = 0
-
+    #lenMenor = 100
     #toda vez que atualizar a pagina inicial, verifica o ping
     for h in hosts:
         verificaServer(h.id)
 
+    #datas = retornaDatas()
+
     #guarda tudo em histTodosHosts
     for e in eventos:
-        histPingHost, dataHora = historicoDePings(e.id)
+        histPingHost, tam = historicoDePings(e.id)
+        #if tam < lenMenor: lenMenor = tam
         if (histPingHost):
+            #if (lenMenor < len(histPingHost)):
+                #histPingHost = histPingHost[lenMenor:]
+
+            histPingSemNone = histPingHost.copy()
+            while None in histPingSemNone:
+                histPingSemNone.remove(None)
             histTodosHosts.append(histPingHost)
-            maxValorGraf.append(max(histPingHost)) #pega o maior valor de ping de cada host
+            maxValorGraf.append(max(histPingSemNone)) #pega o maior valor de ping de cada host
 
     #valores de 'y' e 'passo' do grafico dashboard2.html
     if (maxValorGraf):
-        maxValorGraf = int(max(maxValorGraf)) + 1 #maior valor do vetor + 3
-        passo = int(maxValorGraf / 12)
-    
+        maxValorGraf = int(max(maxValorGraf)) + 2 #maior valor do vetor + 2
+        passo = int(maxValorGraf / 100)
+
     #dashboard.html nao esta sendo utilizado
     listaPing, listaHosts = graficoBarras(eventos)
-    
+
     #zip junta as duas listas para usar no 'for' do grafico
     #pega cada host e seu historico de pings
     graf = zip(listaHosts, histTodosHosts)
 
-    return render(request, 'principal.html', {'eventos': eventos, 'listaPing':listaPing, 'listaHosts':listaHosts, 'dataHora':dataHora, 'graf':graf, 'maxValorGraf':maxValorGraf, 'passo':passo})
+    context = {
+        'eventos': eventos,
+        'listaPing':listaPing,
+        'listaHosts':listaHosts,
+        'graf':graf,
+        'maxValorGraf':maxValorGraf,
+        'passo':passo
+        }
+
+    return render(request, 'principal.html', context)
 
 #grafico de barras dashboard.html nao esta sendo utilizado
 def graficoBarras(eventos):
@@ -55,23 +72,31 @@ def historicoDePings(id):
     evento = Evento.objects.get(pk=id)
     historicoEvento = evento.history.all()
     p = []
-    d = [] #lista com horas para usar no grafico
 
-    for e in historicoEvento:
-        if e.ping == None:
-            pass
-        else:
-            p.append(e.ping)
-        d.append(e.dataHora)
-
-    d.pop() # quando cria um evento, o primeiro nao possui ping
-            # por isso, a primeira data eh omitida
-
-    if len(d) >= 10:
-        d = d[:10]
+    # se nao esta online, nao vai pro grafico
+    estaOn = historicoEvento.first().status
+    if estaOn == 'ONLINE':
+        for e in historicoEvento:
+            if e.status == 'OFFLINE' or '':
+                p.append(None)
+                # if len(p) >= 10:
+                #     p = p[:10]
+                # p.reverse()
+                # return p, len(p)
+            else:
+                p.append(e.ping)
     if len(p) >= 10:
         p = p[:10] #pega apenas os 10 ultimos pings
-    
-    d.reverse()
+
     p.reverse() #envia reverse para mostrar no grafico
-    return p, d
+    return p, len(p)
+
+def retornaDatas():
+    datas = []
+    localhost = Evento.objects.first()
+    histDatas = localhost.history.all()[:10]
+    for d in histDatas:
+        datas.append(d.dataHora)
+    datas.reverse()
+    return datas
+
--- Visualizacao/views.py
+++ Visualizacao/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from Hosts.models import Evento, Host_Porta
 from Hosts.views import verificaServer
-from Principal.views import historicoDePings, graficoBarras
+from Principal.views import historicoDePings, graficoBarras, retornaDatas
 
 @login_required
 def modoVisualizacao(request):
@@ -11,25 +11,29 @@ def modoVisualizacao(request):
     eventosOff = Evento.objects.filter(status="OFFLINE")
     histPingHost = []
     histTodosHosts = []
-    dataHora = []
     maxValorGraf = []
     passo = 0
-
+    lenMenor = 100
     #toda vez que atualizar a pagina inicial, verifica o ping
     for h in hosts:
         verificaServer(h.id)
 
+    #datas = retornaDatas()
+
     #guarda tudo em histTodosHosts
     for e in eventos:
-        histPingHost, dataHora = historicoDePings(e.id)
+        histPingHost, tam = historicoDePings(e.id)
+        if tam < lenMenor: lenMenor = tam
         if (histPingHost):
+            if (lenMenor < len(histPingHost)):
+                histPingHost = histPingHost[lenMenor:]
             histTodosHosts.append(histPingHost)
             maxValorGraf.append(max(histPingHost)) #pega o maior valor de ping de cada host
 
     #valores de 'y' e 'passo' do grafico visualizacaoDashboard.html
     if (maxValorGraf):
-        maxValorGraf = int(max(maxValorGraf)) + 1 #maior valor do vetor + 3
-        passo = int(maxValorGraf / 12)
+        maxValorGraf = int(max(maxValorGraf)) + 2 #maior valor do vetor + 2
+        passo = int(maxValorGraf / 100)
     
     #dashboard.html nao esta sendo utilizado
     listaPing, listaHosts = graficoBarras(eventos)
@@ -38,5 +42,13 @@ def modoVisualizacao(request):
     #pega cada host e seu historico de pings
     graf = zip(listaHosts, histTodosHosts)
 
+    context = {
+        'eventos': eventos,
+        'eventosOff':eventosOff,
+        'listaHosts':listaHosts,
+        'graf':graf,
+        'maxValorGraf':maxValorGraf,
+        'passo':passo
+    }
 
-    return render(request, 'modoVisualizacao.html', {'eventos': eventos, 'eventosOff':eventosOff, 'listaHosts':listaHosts, 'dataHora':dataHora, 'graf':graf, 'maxValorGraf':maxValorGraf, 'passo':passo})
+    return render(request, 'modoVisualizacao.html', context)
